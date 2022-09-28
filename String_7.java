5a9cec905abe84702797f689e7354dfe637d309c
Sathiyanarayanan33 <sathiya.e2000@gmail.com>
2022-09-28 14:23:41 +0530

    first commit

---
+++ String_7.java
@@ -0,0 +1,15 @@
+package string; //we use the array and string method 
+
+public class String_7 {
+
+	public static void main(String[] args) {
+		char c[]= {'a','p','p','l','e'};
+	     for (int i=c.length-1;i>=0;i--) {
+	    	System.out.print(c[i]);
+	}
+     char a[]= {'s','a','t','h','i','y','a'};
+     for(int i=a.length-1; i>=0; i--) {
+    	 System.out.println(a[i]);
+     }
+	}
+}
